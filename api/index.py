5cb61588d8b4e06e12c814efdb3e9fea6acd9705
creuser <151720755+creuserr@users.noreply.github.com>
2024-02-15 19:06:24 +0800

    update index.py

--- api/index.py
+++ api/index.py
@@ -20,7 +20,7 @@ class handler(BaseHTTPRequestHandler):
       req = requests.get(path, proxies=proxy)
       if req.status_code > 399:
         raise Exception(f'{req.status_code}: {req.text}')
-      self.end(200, req.tex)
+      self.end(200, req.text)
     except e:
       return self.end(400, f'400 Bad Request: The requested URL raised an error: {str(e)}')
   
@@ -29,74 +29,3 @@ class handler(BaseHTTPRequestHandler):
     self.send_header('Content-type', 'text/plain')
     self.end_headers()
     self.wfile.write(text.encode('utf-8'))
-
-"""
-class handler(BaseHTTPRequestHandler):
-  def do_POST(self):
-    # check for proxy
-    if 'X-Request-Proxy' in self.headers:
-      # use the given proxy if specified
-      proxy = self.headers['X-Request-Proxy'].split('\n')
-    else:
-      # find proxy source online if not specified
-      proxy = requests.get('https://api.proxyscrape.com/?request=getproxies&proxytype=http&country=all&ssl=all&anonymity=all')
-      proxy = proxy.text.split('\n')
-    proxy = { 'http': proxy }
-    
-    # check for method
-    if 'X-Request-Method' not in self.headers:
-      return self.end(400, '400 Bad Request: X-Request-Method is not specified')
-    method = self.headers['X-Request-Method']
-    
-    # check for url
-    if 'X-Request-URL' not in self.headers:
-      return self.end(400, '400 Bad Request: X-Request-URL is not specified')
-    url = self.headers['X-Request-URL']
-    
-    # check for headers
-    if 'X-Request-Headers' in self.headers:
-      headers = json.loads(self.headers['X-Request-Headers'])
-    else:
-      headers = {}
-    
-    # start request for get
-    if method == 'GET':
-      req = requests.get(url, proxies=proxy, headers=headers)
-      self.send_response(200)
-      self.send_header('Content-type', 'application/json')
-      self.send_header('X-Request-Proxy', json.dumps(proxy['http']))
-      self.end_headers()
-      wfile = {
-        'status_code': req.status_code,
-        'text': req.text,
-        'headers': req.headers
-      }
-      self.wfile.write(json.dumps(wfile).encode('utf-8'))
-      return
-    
-    # start request for post
-    if method == 'POST':
-      req = requests.get(url, proxies=proxy, headers=headers, data=self.rfile)
-      self.send_response(200)
-      self.send_header('Content-type', 'application/json')
-      self.send_header('X-Request-Proxy', json.dumps(proxy['http']))
-      self.end_headers()
-      wfile = {
-        'status_code': req.status_code,
-        'text': req.text,
-        'headers': req.headers
-      }
-      self.wfile.write(json.dumps(wfile).encode('utf-8'))
-      return
-    
-    self.end('400 Bad Request: X-Request-Method specified an unsupported method')
-    
-  def do_GET(self):
-    self.end(400, '400 Bad Request: Invalid method')
-  
-  def end(self, status, text):
-    self.send_response(status)
-    self.send_header('Content-type', 'text/plain')
-    self.end_headers()
-    self.wfile.write(text.encode('utf-8'))
-"""
