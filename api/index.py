7527a9d44ca5db797069ccc863bb01565f3c1077
si delv <151720755+creuserr@users.noreply.github.com>
2024-04-24 06:55:50 +0800

    update index.py

--- api/index.py
+++ api/index.py
@@ -28,30 +28,37 @@ class Rotation:
 
 class handler(BaseHTTPRequestHandler):
   def do_GET(self):
-    # format the provided url
-    path = self.path[1:]
-    if 'http:' in path:
-      path = path.replace('http:', 'http:/')
-    elif 'https:' in path:
-      path = path.replace('https:', 'https:/')
-    else:
-      return self.end(400, '400 Bad Request: Invalid URL')
-    
-    # generate scraped proxies
-    try:
-      prox1 = requests.get('https://api.proxyscrape.com/?request=getproxies&proxytype=http&country=all&ssl=all&anonymity=all')
-      prox2 = requests.get('https://www.proxy-list.download/api/v1/get?type=http')
-      proxy = { 'http': prox1.text.split('\n').extend(prox2.text.split('\n')) }
-    except:
-      self.end(500, '500 Internal Error: Failed to generate scraped proxies')
-    
-    # start the request
+    url = self.url()
+    proxy = self.proxy()
     try:
       h = {
         'Origin': path,
         'User-Agent': Rotation().create()
       }
       req = requests.get(path, proxies=proxy, headers=h)
+      if req.status_code > 399:
+        raise Exception(f'{req.status_code}: {req.text}')
+      self.end(200, req.text, proxy['http'], req.headers)
+    except BaseException as e:
+      return self.end(400, f'400 Bad Request: The request raised an error\n{str(e)}')
+  
+  def do_POST(self):
+    url = self.url()
+    proxy = self.proxy()
+    try:
+      h = {
+        'Origin': path,
+        'User-Agent': Rotation().create()
+      }
+      for header in dict(self.headers):
+        h[header] = self.headers[header]
+      d = None
+      j = None
+      if(h["Content-Type"] == "application/json"):
+        j = json.loads(self.rfile.read(int(h["Content-Length"])))
+      else:
+        d = self.rfile.read()
+      req = requests.get(path, proxies=proxy, headers=h, data=d, json=j)
       if req.status_code > 399:
         raise Exception(f'{req.status_code}: {req.text}')
       self.end(200, req.text, proxy['http'], req.headers)
@@ -69,3 +76,21 @@ class handler(BaseHTTPRequestHandler):
       self.send_header('X-Request-Headers', json.dumps(dict(headers)))
     self.end_headers()
     self.wfile.write(text.encode('utf-8'))
+  
+  def proxy(self):
+    try:
+      prox1 = requests.get('https://api.proxyscrape.com/?request=getproxies&proxytype=http&country=all&ssl=all&anonymity=all')
+      prox2 = requests.get('https://www.proxy-list.download/api/v1/get?type=http')
+      proxy = { 'http': prox1.text.split('\n').extend(prox2.text.split('\n')) }
+      return proxy
+    except:
+      self.end(500, '500 Internal Error: Failed to generate scraped proxies')
+  
+  def url(self):
+    path = self.path[1:]
+    if 'http:' in path:
+      return path.replace('http:', 'http:/')
+    elif 'https:' in path:
+      return path.replace('https:', 'https:/')
+    else:
+      return self.end(400, '400 Bad Request: Invalid URL')
